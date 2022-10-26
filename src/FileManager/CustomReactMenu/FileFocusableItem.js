1b55b2ad43aa27bddd3a86fba1f90567657fd599
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2022-10-26 13:35:27 -0400

    close menu automatically on FileFocusableItem click

--- src/FileManager/CustomReactMenu/FileFocusableItem.js
+++ src/FileManager/CustomReactMenu/FileFocusableItem.js
@@ -3,14 +3,20 @@ import { buttonStyle } from '../utils/constants';
 
 const FileFocusableItem = (props) => {
 	const { icon, title, onClick } = props;
+
+	const handleOnClick = (e, closeMenu) => {
+		onClick(e);
+		closeMenu();
+	};
+
 	return (
 		<FocusableItem>
-			{({ ref }) => (
+			{({ ref, closeMenu }) => (
 				<a
 					ref={ref}
 					className="inline-block rounded hover"
 					title={title}
-					onClick={onClick}
+					onClick={(e) => handleOnClick(e, closeMenu)}
 				>
 					<span className={buttonStyle}>{icon}</span>
 				</a>
