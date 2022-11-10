f8f86de975976e06b710efc622b8abfc35175358
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2022-11-10 00:24:44 -0400

    addition of normal propTypes for regular web component usage

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reactToWebcomponent from 'react-to-webcomponent';
 import FileManager from './FileManager/FileManager';
+import PropTypes from 'prop-types';
 
 // normal development beginning
 const props = {
@@ -25,9 +26,22 @@ root.render(
 reportWebVitals();
 // normal development end
 
-// creating custom element beginning
+// *****************************************************************************************************************
+
+// // creating custom element beginning
+
+// // necessary for exposing attributes for access as a regular web component, without passing in as a property using javascript
+// // note: actions cant be passed in in this way
+// FileManager.propTypes = {
+// 	height: PropTypes.string,
+// 	width: PropTypes.string,
+// 	color: PropTypes.string,
+// 	themeSettings: PropTypes.string,
+// 	backendHostname: PropTypes.string,
+// };
+
 // customElements.define(
-// 	'file-manager',
+// 	'actions-file-manager',
 // 	reactToWebcomponent(FileManager, React, ReactDOM)
 // );
-// creating custom element end
+// // creating custom element end
